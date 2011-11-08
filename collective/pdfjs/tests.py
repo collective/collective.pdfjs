2e23ed65fbb970386c4d68cc3d6c0970e1ea18f5
zedr <zedr@zedr.com>
2011-11-08 09:15:09 +0100

    Working browser test for view

--- collective/pdfjs/tests.py
+++ collective/pdfjs/tests.py
@@ -1,4 +1,5 @@
 import unittest2 as unittest
+import transaction
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
 from plone.testing.z2 import Browser
@@ -13,12 +14,14 @@ class TestSetup(unittest.TestCase):
     layer = PDFJS_INTEGRATION_TESTING
 
     def test__verify_installation(self):
+        """Check if installed"""
         portal = self.layer['portal']
         qi = getToolByName(portal, 'portal_quickinstaller')
         self.assertTrue(
             qi.isProductInstalled('collective.pdfjs'))
 
     def test__verify_my_stylesheets(self):
+        """Check for stylesheets"""
         portal = self.layer['portal']
         css = getToolByName(portal, 'portal_css')
         self.assertTrue(
@@ -26,6 +29,7 @@ class TestSetup(unittest.TestCase):
             in css.getResourceIds())
 
     def test__verify_my_ecmascripts(self):
+        """Check for scripts"""
         portal = self.layer['portal']
         jss = getToolByName(portal, 'portal_javascripts')
         scripts = [
@@ -36,9 +40,13 @@ class TestSetup(unittest.TestCase):
             self.assertTrue(s in jss.getResourceIds())
 
     def test__verify_my_view(self):
-        # Needs finishing...
+        """Check that the 'pdfjs_file_view is installed correctly,
+        and viewable"""
         portal = self.layer['portal']
-        request = self.layer['request']
         setRoles(portal, TEST_USER_ID, ['Manager'])
         portal.invokeFactory('File', 'test_pdf', title=u"Tracemonkey PDF")
-        test_pdf = portal['test_pdf']
+        transaction.commit()
+        browser = Browser(portal)
+        browser.open(portal['test_pdf'].absolute_url() + '/pdfjs_file_view')
+        # A DOM element associated with this view is present
+        self.assertTrue('#controls-wrapper' in browser.contents)
