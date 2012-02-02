0d5f3f7a7556802b309488e273ba0eb2a9f5bd40
Rigel Di Scala <zedr@zedr.com>
2012-02-03 00:15:27 +0100

    Additional fixes for the new viewer

--- collective/pdfjs/browser/ecmascripts/viewer.js
+++ collective/pdfjs/browser/ecmascripts/viewer.js
@@ -2,34 +2,35 @@
 
 'use strict';
 
-var PdfView = function (url) {
-    document.title = this.url = url;
-    var self = this;
-    this.scale = 1.0;
-    this.canvas = document.getElementById('the-canvas');
-    this.context = this.canvas.getContext('2d');
-    this.pagenum = 1;
+var PdfView = function(url) {
+	document.title = this.url = url;
+	var self = this;
+	this.scale = 1.0;
+	this.canvas = document.getElementById('the-canvas');
+	this.context = this.canvas.getContext('2d');
+	this.pagenum = 1;
 
-    PDFJS.getPdf(url, function getPdfData(data) {
-        self.doc = new PDFJS.PDFDoc(data);
-    });
+	PDFJS.getPdf(url, function getPdfData(data) {
+		self.doc = new PDFJS.PDFDoc(data);
+	});
 
-    this.renderPage = function (num) {
-        var page = self.doc.getPage(num);
-        self.canvas.height = page.height * self.scale;
-        self.canvas.width = page.width * self.scale;
-        page.startRendering(self.context);
-    };
+	this.renderPage = function(num) {
+		var page = self.doc.getPage(num);
+		self.canvas.height = page.height * self.scale;
+		self.canvas.width = page.width * self.scale;
+		page.startRendering(self.context);
+	};
 
-    this.renderNextPage = function () {
-        self.renderPage(++this.pagenum);
-    }
+	this.renderNextPage = function() {
+		self.renderPage(++this.pagenum);
+	}
 
-    this.renderPrevPage = function () {
-        self.renderPage(--this.pagenum);
-    }
+	this.renderPrevPage = function() {
+		if (this.pagenum > 1) {
+			self.renderPage(--this.pagenum);
+		}
+	}
 
 }
-
 
 /* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
